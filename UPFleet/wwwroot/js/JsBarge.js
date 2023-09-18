6741e2277ca763737bc4836c0138ab7bcd39a3f0
avadheshcb <codingbrains33@gmail.com>
2023-09-18 22:31:55 +0530

    Change_Aman 18/09/2023

--- UPFleet/wwwroot/js/JsBarge.js
+++ UPFleet/wwwroot/js/JsBarge.js
@@ -64,53 +64,7 @@ $(document).ready(function () {
 
 
 
-    $('#exportBtn').click(function (event) {
-        event.preventDefault(); // Prevent the default form submission behavior
-
-        $.ajax({
-            url: '/Maintenance/ExportBargesToExcel', // Make sure the URL is correct
-            type: 'GET',
-            xhrFields: {
-                responseType: 'blob' // Set the response type to blob
-            },
-            success: function (blob) {
-                // Save the blob as a file using FileSaver.js
-                saveAs(blob, 'Barges.xlsx');
-            },
-
-            error: function (error) {
-                console.error("Error exporting data: ", error);
-            }
-        });
-    });
-
-
-    $('#importBtn').click(function (event) {
-        event.preventDefault();
-        // Prevent the default form submission behavior
-        // Create a hidden file input element
-        var fileInput = document.createElement('input');
-        fileInput.type = 'file';
-        fileInput.accept = '.xlsx, .xls';
-        fileInput.style.display = 'none';
-
-        // Attach change event handler to the hidden input
-        fileInput.addEventListener('change', function () {
-            var file = fileInput.files[0];
-
-            if (file) {
-                var formData = new FormData();
-                formData.append('excelFile', file);
-
-                // Start the import process
-                importBarges(formData);
-            }
-        });
-
-        // Trigger click on the hidden file input
-        fileInput.click();
-    });
-
+  
     $('#nextID').click(function () {
         var id = $('#bargeID').val();
         var actionmessage = "next";
@@ -147,30 +101,7 @@ $(document).ready(function () {
         }
     });
 
-    function importBarges(formData) {
-        $.ajax({
-            url: '/Maintenance/CheckDuplicateBarge',
-            type: 'POST',
-            data: formData,
-            processData: false,
-            contentType: false,
-            beforeSend: function () {
-                $('#loader-overlay').show();  // Show loader before sending request
-            },
-            success: function (response) {
-                $('#loader-overlay').hide();
-                if (response.isDuplicate) {
-                    handleDuplicateBarges(response, formData);
-                } else {
-                    proceedWithImport(formData);
-                }
-            },
-            error: function (error) {
-                $('#loader-overlay').hide();
-                alert("Error impoting Barges: " + error.responseText);
-            }
-        });
-    }
+   
     function filldata(selectedValue) {
 
         // Send the selected value to the MVC action using AJAX
@@ -189,65 +120,8 @@ $(document).ready(function () {
         });
     }
 
-    function handleDuplicateBarges(response, formData) {
-        if (response.totalnewbarges > 0) {
-            Swal.fire({
-                title: response.totalnewbarges + ' New Barges in this file.\n' + response.totalduplicatebarge + ' Duplicate Barges Found.',
-                text: 'Do you want to skip duplicate barges and import or cancel importing?',
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#3085d6',
-                cancelButtonColor: '#d33',
-                confirmButtonText: 'Skip Duplicates and Import',
-                cancelButtonText: 'Cancel Importing'
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    proceedWithImport(formData);
-                }
-                else {
-                    Swal.fire(
-                        'Cancelled',
-                        'Import process canceled.',
-                        'error'
-                    );
-                }
-            })
-        }
-        else {
-            Swal.fire(
-                'No Additional Barges',
-                'All Barges are already Stored. No New Barge Found in this Excel File.',
-                'warning'
-            );
-        }
-    }
+   
 
-    function proceedWithImport(formData) {
-        $.ajax({
-            url: '/Maintenance/ImportBargesFromExcel', // Server URL for import
-            type: 'POST',
-            data: formData,
-            processData: false,
-            contentType: false,
-            beforeSend: function () {
-                $('#loader-overlay').show(); // Show loader before sending request
-            },
-            success: function (response) {
-                $('#loader-overlay').hide();
-                // Handle successful import response
-                alert("Import successful:", response);
-            },
-            error: function (error) {
-                $('#loader-overlay').hide();
-                // Show an alert for other errors
-                Swal.fire(
-                    'Error importing data:',
-                    '\n' + error.responseText,
-                    'error'
-                );
-            }
-        });
-    }
 
 
     function validateBargeName() {
--- UPFleet/wwwroot/js/JsBargeReport.js
+++ UPFleet/wwwroot/js/JsBargeReport.js
@@ -16,7 +16,135 @@
         const SelectOwner = $('#ownerDropdown').val();
         LoadBarges(SelectOwner);
     });
+    $('#exportBtn').click(function (event) {
+        event.preventDefault(); // Prevent the default form submission behavior
 
+        $.ajax({
+            url: '/Maintenance/ExportBargesToExcel', // Make sure the URL is correct
+            type: 'GET',
+            xhrFields: {
+                responseType: 'blob' // Set the response type to blob
+            },
+            success: function (blob) {
+                // Save the blob as a file using FileSaver.js
+                saveAs(blob, 'Barges.xlsx');
+            },
+
+            error: function (error) {
+                console.error("Error exporting data: ", error);
+            }
+        });
+    });
+
+
+    $('#importBtn').click(function (event) {
+        event.preventDefault();
+        // Prevent the default form submission behavior
+        // Create a hidden file input element
+        var fileInput = document.createElement('input');
+        fileInput.type = 'file';
+        fileInput.accept = '.xlsx, .xls';
+        fileInput.style.display = 'none';
+
+        // Attach change event handler to the hidden input
+        fileInput.addEventListener('change', function () {
+            var file = fileInput.files[0];
+
+            if (file) {
+                var formData = new FormData();
+                formData.append('excelFile', file);
+
+                // Start the import process
+                importBarges(formData);
+            }
+        });
+
+        // Trigger click on the hidden file input
+        fileInput.click();
+    });
+    function importBarges(formData) {
+        $.ajax({
+            url: '/Maintenance/CheckDuplicateBarge',
+            type: 'POST',
+            data: formData,
+            processData: false,
+            contentType: false,
+            beforeSend: function () {
+                $('#loader-overlay').show();  // Show loader before sending request
+            },
+            success: function (response) {
+                $('#loader-overlay').hide();
+                if (response.isDuplicate) {
+                    handleDuplicateBarges(response, formData);
+                } else {
+                    proceedWithImport(formData);
+                }
+            },
+            error: function (error) {
+                $('#loader-overlay').hide();
+                alert("Error impoting Barges: " + error.responseText);
+            }
+        });
+    }
+
+    function proceedWithImport(formData) {
+        $.ajax({
+            url: '/Maintenance/ImportBargesFromExcel', // Server URL for import
+            type: 'POST',
+            data: formData,
+            processData: false,
+            contentType: false,
+            beforeSend: function () {
+                $('#loader-overlay').show(); // Show loader before sending request
+            },
+            success: function (response) {
+                $('#loader-overlay').hide();
+                // Handle successful import response
+                alert("Import successful:", response);
+            },
+            error: function (error) {
+                $('#loader-overlay').hide();
+                // Show an alert for other errors
+                Swal.fire(
+                    'Error importing data:',
+                    '\n' + error.responseText,
+                    'error'
+                );
+            }
+        });
+    }
+    function handleDuplicateBarges(response, formData) {
+        if (response.totalnewbarges > 0) {
+            Swal.fire({
+                title: response.totalnewbarges + ' New Barges in this file.\n' + response.totalduplicatebarge + ' Duplicate Barges Found.',
+                text: 'Do you want to skip duplicate barges and import or cancel importing?',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Skip Duplicates and Import',
+                cancelButtonText: 'Cancel Importing'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    proceedWithImport(formData);
+                }
+                else {
+                    Swal.fire(
+                        'Cancelled',
+                        'Import process canceled.',
+                        'error'
+                    );
+                }
+            })
+        }
+        else {
+            Swal.fire(
+                'No Additional Barges',
+                'All Barges are already Stored. No New Barge Found in this Excel File.',
+                'warning'
+            );
+        }
+    }
     function LoadOwners() {
         const dropdownOwner = $('#ownerDropdown');
 
